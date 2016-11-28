b56ae3e6a6818d3219d905c8b4f95423386aab28
Connor <connorpeshek@gmail.com>
2016-11-28 06:07:55 -0500

    a

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   entry: [
-    '/src/index.js'
+    path.join(__dirname, "src", "index.js")
   ],
   output: {
     path: path.join(__dirname, "public"),
