7c3022503fa2c3bc3ea2498f1d763049bd58775a
Connor <connorpeshek@gmail.com>
2016-11-29 18:30:22 -0500

    Change google map plugin for heroku support.

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import {GoogleMapLoader, GoogleMap} from 'react-google-maps'
+// import {GoogleMapLoader, GoogleMap} from 'react-google-maps'
+import GoogleMap from 'google-map-react'
+
+const GOOGLE_API_KEY = 'AIzaSyA-lkF4xeNb7fY_pQTsFIApa6l0f-TZMQs' 
+
+// export default (props) => {
+//   return (
+//     <GoogleMapLoader
+//     containerElement={ <div style={{height: '100%'}} />}
+//     googleMapElement={
+//       <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
+//     }
+//     />
+//   )
+// }
 
 export default (props) => {
   return (
-    <GoogleMapLoader
-    containerElement={ <div style={{height: '100%'}} />}
-    googleMapElement={
-      <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
-    }
-    />
+    <GoogleMap
+      apiKey={GOOGLE_API_KEY}
+      bootstrapURLKeys={{key: GOOGLE_API_KEY}}
+      zoom={12}
+      center={[props.lat, props.lon]}>
+    </GoogleMap>
   )
 }
